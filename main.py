48701c21a96a7376d1a5f96a2cb7633f4c495b82
RossB92 <rosswbillington@gmail.com>
2023-09-28 08:41:20 -0500

    Create word popup and timer
    Create a pop up for n random words and set up a timer to time they typing

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 from tkinter import *
 import nltk
+import math
 
 # nltk.download("gutenberg")
 from nltk.corpus import gutenberg
 import random
 
+# ------------------ Random Words -----------------------#
+
 moby = set(nltk.Text(gutenberg.words("melville-moby_dick.txt")))
 moby = [word.lower() for word in moby if len(word) > 2]
+
+
+def start_test():
+    words_to_type = Label(text=random_word_list, wraplength=250)
+    words_to_type.grid(column=2, row=2)
+    count_down(60)
+
+
+# -------------------COUNTDOWN TIMER --------------------#
+def count_down(count):
+    # global reps
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    timer_text = f"{count_min}:{count_sec}"
+    canvas.itemconfig(timer_text_canvas, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+
+
+# --------------------- TIMER MECHANISM -----------------#
 
 
 # -------------------- CONSTANTS ------------------------#
@@ -15,8 +41,9 @@ random_word_list = []
 for i in range(0, 51):
     random_word = moby[int(random.random() * len(set(moby)))]
     random_word_list.append(random_word)
-
-# -------------------------------------------------------#
+reps = 0
+timer = None
+TYPING_TIME = 60
 
 
 # -------------------- UI SETUP -------------------------#
@@ -26,10 +53,21 @@ window.config(padx=100, pady=50, bg=GREY_BLUE)
 
 canvas = Canvas(width=800, height=600, bg=GREY_BLUE, highlightthickness=0)
 
-words_to_type = Label(text=random_word_list, wraplength=250)
-words_to_type.grid(column=2, row=0)
+# Start Typing Button
+start_typing_button = Button(text="Start Typing Test", width=15, command=start_test)
+start_typing_button.grid(column=2, row=0)
 
+# Timer Text
+timer_text = Label(text="00:00")
+timer_text.grid(column=2, row=1)
+# timer_text = canvas.create_text(10, 30, text="00:00")
+# canvas.grid(column=2, row=1)
+
+# Typing Area
 typing_area = Entry(width=52)
-typing_area.grid(column=2, row=1)
+typing_area.grid(column=2, row=3)
+
+timer_text_canvas = canvas.create_text(100, 130, text="00:00")
+canvas.grid(column=2, row=5)
 
 window.mainloop()
